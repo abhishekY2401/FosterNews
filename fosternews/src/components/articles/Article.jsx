e34f5095baf6029f6322c823a71463cf042e9133
Abhishek Yadav <abhiyd2401@gmail.com>
2023-06-24 17:40:41 +0530

    fetching data from scraper url

--- fosternews/src/components/articles/Article.jsx
+++ fosternews/src/components/articles/Article.jsx
@@ -4,24 +4,24 @@ const Article = () => {
   const location = useLocation();
   const {article} = location.state;
 
-  const {title, content, source, image, publishedAt} = article;
-  const {name} = source;
+  const {title, content, img_url} = article;
+  const heading = title.replace("'", " ");
 
   return (
     <div>
       <div className="h-72 flex justify-center items-center bg-black pr-72 pl-72">
-        <p className="text-white font-playfair text-center text-5xl">{title}</p>
+        <p className="text-white font-playfair text-center text-5xl">{heading}</p>
       </div>
       <div className="pr-60 pl-60 flex flex-col justify-between gap-10">
         <div className="pt-10 flex items-center gap-2 text-black text-base font-quicksand">
-          <p>{name}</p>
+          <p></p>
           <hr className="h-0.5 w-2 bg-black" />
-          <p>{new Date(publishedAt).getFullYear()}</p>
+          <p>{new Date().getFullYear()}</p>
         </div>
         
         <hr className="h-0.5 font-bold bg-gray-400"/>
         <div>
-            <img src={image} alt="imgs" />
+            <img src={img_url} alt="imgs" className="w-full filter contrast-150"/>
         </div>
         <div className="font-play text-xl font-normal">{content}</div>
       </div>
